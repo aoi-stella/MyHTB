76b2e162907e15a2c38c64eb0adbe783f632efba
Hal <halcorder@gmail.com>
2023-05-15 06:02:39 +0900

    不要コード削除

--- src/app/src/androidTest/java/com/example/myhtb/view/login/LoginFragmentViewTest.kt
+++ src/app/src/androidTest/java/com/example/myhtb/view/login/LoginFragmentViewTest.kt
@@ -1,22 +1,14 @@
 package com.example.myhtb.view.login
 
-import android.util.Log
-import android.view.View
-import android.view.ViewGroup
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
 import com.example.myhtb.R
 import com.example.myhtb.view.main.MainActivityView
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.Matchers
-import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -85,23 +77,5 @@ class LoginPage {
             .check(matches(isDisplayed()))
             .check(matches(withText(correctConnectionStatus)))
         return this
-    }
-
-    private fun childAtPosition(
-        parentMatcher: Matcher<View>, position: Int
-    ): Matcher<View> {
-
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("Child at position $position in parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            public override fun matchesSafely(view: View): Boolean {
-                val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
-            }
-        }
     }
 }
