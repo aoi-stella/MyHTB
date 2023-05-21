cd18d081678e9d1d315b19966b32c6fd4b79a6d2
Hal <halcorder@gmail.com>
2023-05-21 22:54:35 +0900

    コメントを追加した

--- src/app/src/androidTest/java/com/example/myhtb/common/uiUtils.kt
+++ src/app/src/androidTest/java/com/example/myhtb/common/uiUtils.kt
@@ -1,19 +1,26 @@
 package com.example.myhtb.common
 
-import android.app.Instrumentation
-import android.service.autofill.FieldClassification.Match
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.BySelector
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.Until
-import kotlinx.coroutines.selects.select
-import okhttp3.internal.wait
 import org.hamcrest.Matchers
 
+/**
+ * uiのユニットテストに関する処理をまとめたもの
+ * インスタンス化する必要は特にないためシングルトン構成とする
+ */
 object uiUtils {
+    /** uiDevice **/
     private val uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
 
+    /**
+     * 指定のウィジェットが現れるまで待機する。非同期処理などで使用する
+     *
+     * @param widget 対象のウィジェット
+     * @param timeoutMs タイムアウト時間(単位はミリセカンド)
+     */
     fun waitForSpecifiedWidget(widget : BySelector, timeoutMs : Long = 10000){
         ViewMatchers.assertThat(
             uiDevice.wait(
