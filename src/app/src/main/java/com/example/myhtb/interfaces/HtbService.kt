b2df07eda95e2d9faaef8e9c3a1e0750cd21b1aa
hal-art <halcorder@gmail.com>
2023-06-13 22:58:41 +0900

    Retrofit用のI/Fを追加した

---
+++ src/app/src/main/java/com/example/myhtb/interfaces/HtbService.kt
@@ -0,0 +1,23 @@
+package com.example.myhtb.interfaces
+
+import retrofit2.Response
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+/**
+ * EPを定義
+ */
+private object EP{
+    const val LOGIN = "/api/v4/login"
+}
+
+interface HtbService {
+    @FormUrlEncoded
+    @POST(EP.LOGIN)
+    suspend fun login(
+        @Field("email") email: String,
+        @Field("password") password: String,
+        @Field("remember") remember: Boolean
+    ): String
+}
