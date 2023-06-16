a04097981e1ebb82cf72c8fe2659f9d7825db48b
hal-art <halcorder@gmail.com>
2023-06-16 14:58:49 +0900

    LoginFragmentModel.ktにログ機能追加した

--- src/app/src/main/java/com/example/myhtb/model/login/LoginFragmentModel.kt
+++ src/app/src/main/java/com/example/myhtb/model/login/LoginFragmentModel.kt
@@ -1,5 +1,6 @@
 package com.example.myhtb.model.login
 
+import com.example.myhtb.logger.Logger
 import com.example.myhtb.repository.HtbRepository
 
 /**
@@ -9,6 +10,11 @@ import com.example.myhtb.repository.HtbRepository
  * 他Modelクラスからのアクセスも考慮してシングルトンこう制とする
  */
 object LoginFragmentModel{
+    /**
+     * タグ名
+     */
+    private var TAG = this::class.java.simpleName
+
     /**
      * アクセストークン
      */
@@ -23,14 +29,21 @@ object LoginFragmentModel{
      * @param password ログイン用パスワード
      */
     suspend fun LoginToHackTheBox(email: String, password: String) : Boolean{
+        Logger.LogDebug(TAG, "Start LoginToHackTheBox")
+
+        var result: Boolean
         val token = HtbRepository.Login(email, password)
         if (token != "" && token != null){
+            Logger.LogDebug(TAG, "Succeed to fetch access token")
             _accessToken = token
-            return true
+            result = true
         }
         else{
+            Logger.LogDebug(TAG, "Failed to fetch access token")
             _accessToken = ""
-            return false
+            result = false
         }
+        Logger.LogDebug(TAG, "Finish LoginToHackTheBox")
+        return result
     }
 }
