acb2014db393f61e9370fd428a60614effcea155
hal-art <halcorder@gmail.com>
2023-06-25 16:03:13 +0900

    ユーザー情報画面をデータバインディングに対応するようにした

--- src/app/src/main/java/com/example/myhtb/view/userinfo/UserInfoFragmentView.kt
+++ src/app/src/main/java/com/example/myhtb/view/userinfo/UserInfoFragmentView.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.myhtb.R
+import com.example.myhtb.databinding.FragmentLoginBinding
+import com.example.myhtb.databinding.FragmentUserInfoBinding
 import com.example.myhtb.logger.Logger
+import com.example.myhtb.viewmodel.login.LoginFragmentViewModel
+import com.example.myhtb.viewmodel.userinfo.UserInfoFragmentViewModel
 
 /**
  * ユーザー情報画面のViewクラス
@@ -16,6 +22,11 @@ class UserInfoFragmentView : Fragment() {
      * タグ名
      */
     private var TAG = this::class.java.simpleName
+
+    /**
+     * LoginFragmentのViewModel
+     */
+    private val vm : UserInfoFragmentViewModel by viewModels()
 
     /**
      * onCreateView処理
@@ -29,7 +40,11 @@ class UserInfoFragmentView : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         Logger.LogDebug(TAG, "Start onCreateView")
+        super.onCreateView(inflater, container, savedInstanceState)
+        val binding = DataBindingUtil.inflate<FragmentUserInfoBinding>(inflater, R.layout.fragment_user_info, container, false)
+        binding.vm = vm
+        binding.lifecycleOwner = this
         Logger.LogDebug(TAG, "Finish onCreateView")
-        return inflater.inflate(R.layout.fragment_user_info, container, false)
+        return binding.root
     }
 }
--- src/app/src/main/java/com/example/myhtb/viewmodel/userinfo/UserInfoFragmentViewModel.kt
+++ src/app/src/main/java/com/example/myhtb/viewmodel/userinfo/UserInfoFragmentViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.myhtb.viewmodel.userinfo
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class UserInfoFragmentViewModel : ViewModel() {
@@ -9,4 +10,8 @@ class UserInfoFragmentViewModel : ViewModel() {
     private var TAG = this::class.java.simpleName
 
 
+    /**
+     * ユーザー名
+     */
+    val userName = MutableLiveData("test")
 }
